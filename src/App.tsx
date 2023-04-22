e0347e4ce57ebf4fa072e5d46f935b6b3343f400
YSRKEN <ysr.ken@gmail.com>
2023-04-23 05:22:48 +0900

    Update SettingSlider

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,27 @@ const AV_LIST = [
   { label: "F6.3", value: 16 },
   { label: "F7.1", value: 17 },
   { label: "F8.0", value: 18 },
+  { label: "F9.0", value: 19 },
+  { label: "F10", value: 20 },
+  { label: "F11", value: 21 },
+  { label: "F13", value: 22 },
+  { label: "F14", value: 23 },
+  { label: "F16", value: 24 },
+  { label: "F18", value: 25 },
+  { label: "F20", value: 26 },
+  { label: "F22", value: 27 },
 ]
 
 const TV_LIST = [
+  { label: "8.0s", value: -9 },
+  { label: "6.4s", value: -8 },
+  { label: "5.0s", value: -7 },
+  { label: "4.0s", value: -6 },
+  { label: "3.2s", value: -5 },
+  { label: "2.5s", value: -4 },
+  { label: "2.0s", value: -3 },
+  { label: "1.6s", value: -2 },
+  { label: "1.3s", value: -1 },
   { label: "1.0s", value: 0 },
   { label: "0.8s", value: 1 },
   { label: "0.6s", value: 2 },
@@ -43,9 +61,33 @@ const TV_LIST = [
   { label: "1/40s", value: 16 },
   { label: "1/50s", value: 17 },
   { label: "1/60s", value: 18 },
+  { label: "1/80s", value: 19 },
+  { label: "1/100s", value: 20 },
+  { label: "1/125s", value: 21 },
+  { label: "1/160s", value: 22 },
+  { label: "1/200s", value: 23 },
+  { label: "1/250s", value: 24 },
+  { label: "1/320s", value: 25 },
+  { label: "1/400s", value: 26 },
+  { label: "1/500s", value: 27 },
+  { label: "1/640s", value: 28 },
+  { label: "1/800s", value: 29 },
+  { label: "1/1000s", value: 30 },
+  { label: "1/1250s", value: 31 },
+  { label: "1/1600s", value: 32 },
+  { label: "1/2000s", value: 33 },
+  { label: "1/2500s", value: 34 },
+  { label: "1/3200s", value: 35 },
+  { label: "1/4000s", value: 36 },
+  { label: "1/5000s", value: 37 },
+  { label: "1/6400s", value: 38 },
+  { label: "1/8000s", value: 39 },
 ]
 
 const ISO_LIST = [
+  { label: "ISO50", value: -3 },
+  { label: "ISO64", value: -2 },
+  { label: "ISO80", value: -1 },
   { label: "ISO100", value: 0 },
   { label: "ISO125", value: 1 },
   { label: "ISO160", value: 2 },
@@ -65,6 +107,18 @@ const ISO_LIST = [
   { label: "ISO4000", value: 16 },
   { label: "ISO5000", value: 17 },
   { label: "ISO6400", value: 18 },
+  { label: "ISO8000", value: 19 },
+  { label: "ISO10000", value: 20 },
+  { label: "ISO12800", value: 21 },
+  { label: "ISO16000", value: 22 },
+  { label: "ISO20000", value: 23 },
+  { label: "ISO25600", value: 24 },
+  { label: "ISO32000", value: 25 },
+  { label: "ISO40000", value: 26 },
+  { label: "ISO51200", value: 27 },
+  { label: "ISO64000", value: 28 },
+  { label: "ISO80000", value: 29 },
+  { label: "ISO102400", value: 30 },
 ]
 
 const EV_LIST = [
@@ -106,23 +160,24 @@ const SettingSlider = ({ label, values, value, setValue }: {
   })
 
   return <Form.Group>
-    <Form.Label className="fw-bold">{label} ({values[index].label})</Form.Label>
+    <div className="d-flex justify-content-between">
+      <Form.Label>{values[0].label}</Form.Label>
+      <Form.Label className="fw-bold">{label} ({values[index].label})</Form.Label>
+      <Form.Label>{values[values.length - 1].label}</Form.Label>
+    </div>
     <Form.Range min="0" max={values.length - 1} value={index} onChange={(e) => {
       const i = parseInt(e.target.value)
       setIndex(i)
       setValue(values[i].value)
     }} />
-    <div className="d-flex justify-content-between">
-      {values.map((v, i) => <div key={i}>{v.label}</div>)}
-    </div>
   </Form.Group>
 
 }
 
 const App = () => {
 
-  const [fValue, setFValue] = useState(9)
-  const [tValue, setTValue] = useState(18)
+  const [fValue, setFValue] = useState(18)
+  const [tValue, setTValue] = useState(27)
   const [isoValue, setIsoValue] = useState(0)
   const [evValue, setEvValue] = useState(0)
 
@@ -130,7 +185,7 @@ const App = () => {
   console.log('F値', fValue)
   console.log('シャッタースピード', tValue)
   console.log('ISO感度', isoValue)
-  console.log('EV', (fValue + tValue - isoValue - evValue) / 3)
+  console.log('EV', Math.round((fValue + tValue - isoValue - evValue) / 3 * 10) / 10)
   console.groupEnd()
 
   return <Container>
